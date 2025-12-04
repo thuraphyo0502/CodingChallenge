545e6a24cc49b80e541b30eab3218d36b83d9bba
thuraphyo <thuraphyo@mingalarinfotech.com>
2025-12-04 22:32:26 +0700

    add: added the unit tests for old phone keypad

---
+++ CodingChallengeTests/Modules/OldPhoneKeyPadTests.cs
@@ -0,0 +1,55 @@
+﻿using CodingChallenge.Modules;
+
+namespace CodingChallengeTests.Modules
+{
+    public class OldPhoneKeyPadTests
+    {
+        [Fact]
+        public void ShouldIgnoreDelaysAndResetKeyPress()
+        {
+            string input = "222 2 22#";
+            string result = OldPhoneKeyPad.ConvertToText(input);
+            Assert.Equal("CAB", result);
+        }
+
+        [Fact]
+        public void ShouldConvertNumbersToLetters()
+        {
+            string input = "33#";
+            string result = OldPhoneKeyPad.ConvertToText(input);
+            Assert.Equal("E", result);
+        }
+
+        [Fact]
+        public void ShouldHandleBackspace()
+        {
+            string input = "227*#";
+            string result = OldPhoneKeyPad.ConvertToText(input);
+            Assert.Equal("B", result);
+        }
+
+        [Fact]
+        public void ShouldHandleMultiKeySequences()
+        {
+            string input = "4433555 555666096667775553#";
+            string result = OldPhoneKeyPad.ConvertToText(input);
+            Assert.Equal("HELLO WORLD", result);
+        }
+
+        [Fact]
+        public void ShouldHandleMultiKeySequencesWithBackspace()
+        {
+            string input = "8 88777444666*664#";
+            string result = OldPhoneKeyPad.ConvertToText(input);
+            Assert.Equal("TURING", result);
+        }
+
+        [Fact]
+        public void ShouldHandleInvalidInput()
+        {
+            string input = string.Empty;
+            string result = OldPhoneKeyPad.ConvertToText(input);
+            Assert.Equal(string.Empty, result);
+        }
+    }
+}
